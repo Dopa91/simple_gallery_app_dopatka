1bf79b14442ce5b843fa917654c65fec3b9728db
Dopa91 <code.dopatka@gmail.com>
2024-10-08 13:26:54 +0200

    created class

---
+++ lib/features/gallery/models/gallery_item.dart
@@ -0,0 +1,12 @@
+class PictureItem {
+  String imageTitle;
+  String imageDate;
+  String imageDescription;
+  String imagePath;
+
+  PictureItem(
+      {required this.imageDate,
+      required this.imageDescription,
+      required this.imagePath,
+      required this.imageTitle});
+}
