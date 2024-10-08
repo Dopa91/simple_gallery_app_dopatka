d78a9ca11a927d1f45ab30651723a1d8b148f195
Dopa91 <code.dopatka@gmail.com>
2024-10-08 15:40:02 +0200

    created widget

---
+++ lib/features/startpage/widgets/image_slider.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+
+class ImageSlider extends StatelessWidget {
+  const ImageSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ImageSlideshow(
+      disableUserScrolling: true,
+      height: 512,
+      indicatorRadius: 0,
+      isLoop: true,
+      autoPlayInterval: 4200,
+      children: [
+        Image.asset(
+          "assets/images/basketball_1.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/basketball_2.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/bike.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/running.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/ski.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/soccer.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/tennis_1.jpeg",
+          fit: BoxFit.cover,
+        ),
+        Image.asset(
+          "assets/images/tennis_2.jpeg",
+          fit: BoxFit.cover,
+        ),
+      ],
+    );
+  }
+}
